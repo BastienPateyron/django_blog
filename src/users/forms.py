c99b2a8b26490d04daeaaa1f4fd07aec677dc951
bapateyron <bastien.pateyron@etu.isima.fr>
2020-04-03 21:58:51 +0200

    Formulaire d'inscription OK

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,3 +1,12 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-# TODO
+class UserRegisterForm(UserCreationForm):
+   email = forms.EmailField()
+
+   # Les champs avec lesquels on compte intéragir
+   # Par exemple, le champ 'email' ne fonctionne pas si on ne fait pas ça
+   class Meta:
+      model = User
+      fields = ['username', 'email', 'password1', 'password2']
