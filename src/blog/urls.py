75899d928b1ffe6fa20a80a06c93c58d495a4800
bapateyron <bastien.pateyron@etu.isima.fr>
2020-04-10 22:42:33 +0200

    Create Post OK

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
+from .views import (
+   PostListView, 
+   PostDetailView,
+   PostCreateView
+)
 from . import views
 # Le ' . ' fait référence au répertoire courant de ce fichier, donc de blog/
 
 # Un path vide comme suit: '' pointe vers la racine du site
 urlpatterns = [
-   path('', views.home, name = 'blog-home'),
+   path('', PostListView.as_view(), name = 'blog-home'),
+   path('post/<int:pk>/', PostDetailView.as_view(), name = 'post-detail'),
+   path('post/new/', PostCreateView.as_view(), name = 'post-create'),
    path('about/', views.about, name = 'blog-about'),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import (
+   ListView, 
+   DetailView, 
+   CreateView
+)
 from .models import Post
+from django.contrib import messages
+
 # On met ".models" car le fichier est dans le même répertoire que la vue
 
 # Une fonction correspond à une vue
@@ -10,8 +17,36 @@ def home(request):
    return render(request, 'blog/home.html', context)
 
 
+class PostListView(ListView):
+   model = Post
+   template_name = 'blog/home.html'
+   context_object_name = 'posts'    # Les objets seront contenus dans la variable 'posts'
+
+   ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+   model = Post
+   
+
+class PostCreateView(CreateView):
+   model = Post
+   fields = ['title', 'content']
+
+   def form_valid(self, form):
+      form.instance.author = self.request.user # On définit l'auteur avant validation
+      messages.success(self.request, f'Post Created !')
+      return super().form_valid(form)
+
+
 def about(request):
    return render(request, 'blog/about.html', {'title': 'About'})
+
+
+
+
+
+
 
 
 
